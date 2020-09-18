618078e6973ace0626529e27167a17418a3c2c0a
redazures <jwu_nyc@yahoo.com>
2020-09-17 23:47:03 -0400

    2 answers

--- src/App.js
+++ src/App.js
@@ -7,26 +7,31 @@ class App extends React.Component{
         data:[]
     }
 
+    renderPeople = ()=>{
+        console.log("this is render people", this.state)
+        return this.state.data.people.map(person=><p key={person.name}>{person.name}</p>)
+    }
 
+    renderPeople2=()=>{
+        console.log("this is render people 2", this.state)
+        return this.state.data.map((person, id) => <h1 key={id}>{person.name}</h1>)
+    }
+    
     componentDidMount(){
         fetch('http://api.open-notify.org/astros.json')
         .then(res=>res.json())
-        .then(string=>(this.setState({data:string})))
+        // .then(string=>(this.setState({data:string})))
+        .then(string=>(this.setState({data:string.people})))
     }
     
-    renderPeople = ()=>{
-        console.log("this is render people", this.state.data.people)
-    return this.state.data.people.map(person=><p key={person.name}>{person.name}</p>)
-    }
-
     render(){
         return(
             <div>
-                {this.state.data.length===0 ? <h1>Loading</h1> : <h1>{this.renderPeople()}</h1> }
-            {/* {this.state.peopleArray.map((person, id) => <h1 key={id}>{person.name}</h1>)} */}
+                {/* {this.state.data.length===0 ? <h1>Loading</h1> : <h1>{this.renderPeople()}</h1> } */}
+                {this.renderPeople2()}
             </div>
         )
-            
+        
     }
 }
 
